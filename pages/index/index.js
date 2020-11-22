6064a0899a359770d39b33d22b9eb0f542b72ae7
宫小白呀 <58649945+gong9@users.noreply.github.com>
2020-11-23 06:54:38 +0800

    pro-init

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    test: 'Hello World',
+    arr: [{
+      id: 1,
+      name: 'zs'
+    }, {
+      id: 2,
+      name: 'ls'
+    }],
+    num:0
+  },
+  
+})
